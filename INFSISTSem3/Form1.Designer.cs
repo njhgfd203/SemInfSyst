4e3bb9515e20375e17f314ceb30fba8fd7acb847
njhgfd203 <80156900+njhgfd203@users.noreply.github.com>
2025-05-21 00:05:29 +0300

    Add files via upload

---
+++ INFSISTSem3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace INFSISTSem3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.БД = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 393);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(933, 46);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(100, 22);
+            this.txtUserId.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(1155, 46);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(879, 163);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(100, 22);
+            this.txtDatabase.TabIndex = 3;
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(1178, 169);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(100, 22);
+            this.txtTableName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(795, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Имя пользователя ";
+            // 
+            // БД
+            // 
+            this.БД.AutoSize = true;
+            this.БД.Location = new System.Drawing.Point(778, 169);
+            this.БД.Name = "БД";
+            this.БД.Size = new System.Drawing.Size(25, 16);
+            this.БД.TabIndex = 6;
+            this.БД.Text = "БД";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1067, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Пароль";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1082, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Таблица";
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(1203, 282);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadData.TabIndex = 9;
+            this.btnLoadData.Text = "Выполнить";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1389, 820);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.БД);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtTableName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label БД;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLoadData;
+    }
+}
+
---
+++ INFSISTSem3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INFSISTSem3
+{
+    public partial class Form1 : Form
+    {
+        private Database DB;
+        private DataTable table;
+        private MySqlDataAdapter adapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            table = new DataTable();
+            adapter = new MySqlDataAdapter();
+            dataGridView1.DataSource = table;
+
+            btnLoadData.Click += btnLoadData_Click;
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            DB = new Database(txtDatabase.Text, txtUserId.Text, txtPassword.Text);
+            LoadDataFromDatabase(txtTableName.Text);
+        }
+
+        private void LoadDataFromDatabase(string tableName)
+        {
+            DB.OpenConnection();
+
+            string query = $"SELECT * FROM `{DB.GetConnection().Database}`.`{tableName}`";
+            MySqlCommand command = new MySqlCommand(query, DB.GetConnection());
+
+            table.Clear();
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = table;
+
+            DB.CloseConnection();
+        }
+
+        private class Database
+        {
+            private readonly MySqlConnection connection;
+
+            public Database(string database, string userId, string password)
+            {
+                connection = new MySqlConnection(
+                    $"Server=localhost;Database={database};User ID={userId};Password={password};");
+            }
+
+            public void OpenConnection() => connection.Open();
+            public void CloseConnection() => connection.Close();
+            public MySqlConnection GetConnection() => connection;
+        }
+    }
+}
